8d14313b7e4bfd7b066520111b0431baaa88caf4
naekang <rlawlsgh6306@gmail.com>
2022-03-26 23:22:48 +0900

    router 만들기

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -11,34 +11,53 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
-window.addEventListener('hashchange', function () {
-    const id = location.hash.substr(1);
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
+    newsList.push('<ul>');
 
-    title.innerHTML = newsContent.title;
-
-    content.appendChild(title);
-});
-
-for (let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
+    for (let i = 0; i < 10; i++) {
+        newsList.push(`
         <li>
             <a href="#${newsFeed[i].id}">
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>
-    `;
+    `);
+    }
 
-    ul.appendChild(div.firstElementChild);
+    newsList.push('</ul>');
+
+    container.innerHTML = newsList.join('');
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+    const id = location.hash.substr(1);
 
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+        
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+
+function router() {
+    const routePath = location.hash;
+
+    if (routePath === '') {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
